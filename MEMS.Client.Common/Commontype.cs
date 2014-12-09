18500ae21b28f6684a5907e73f867e68393c3dd8
jaypublic <jaypublic@163.com>
2014-12-09 21:54:32 +0800

    增加窗体模式

--- MEMS.Client.Common/Commontype.cs
+++ MEMS.Client.Common/Commontype.cs
@@ -5,11 +5,32 @@ using System.Text;
 
 namespace MEMS.Client.Common
 {
+    /// <summary>
+    /// 窗体模式
+    /// </summary>
     public enum frmmodetype
     {
+        /// <summary>
+        /// 添加模式
+        /// </summary>
         add,
+        /// <summary>
+        /// 编辑模式
+        /// </summary>
         edit,
-        delete
+        /// <summary>
+        /// 删除模式
+        /// </summary>
+        delete,
+        /// <summary>
+        /// 查看模式
+        /// </summary>
+        view,
+        /// <summary>
+        /// 自定义模式
+        /// </summary>
+        custom
+
     }
     class Commontype
     {
