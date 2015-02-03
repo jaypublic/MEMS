6f449a7ed09f370e6c512c76edae047c1b4760f1
jaypublic <jaypublic@163.com>
2015-02-03 22:09:45 +0800

    添加生产模块，创建生产指令单查询界面

---
+++ MEMS.Client.Produce/FormPInsList.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace MEMS.Client.Produce
+{
+    partial class FormPInsList
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            ((System.ComponentModel.ISupportInitialize)(this.searchgroup)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ribboncontrol)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // searchgroup
+            // 
+            this.searchgroup.Size = new System.Drawing.Size(763, 100);
+            // 
+            // ribboncontrol
+            // 
+            this.ribboncontrol.ExpandCollapseItem.Id = 0;
+            this.ribboncontrol.Size = new System.Drawing.Size(763, 125);
+            this.ribboncontrol.Toolbar.ShowCustomizeItem = false;
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 225);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.MenuManager = this.ribboncontrol;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(763, 244);
+            this.gridControl1.TabIndex = 3;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // FormPInsList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.ClientSize = new System.Drawing.Size(763, 469);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "FormPInsList";
+            this.Controls.SetChildIndex(this.ribboncontrol, 0);
+            this.Controls.SetChildIndex(this.searchgroup, 0);
+            this.Controls.SetChildIndex(this.gridControl1, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.searchgroup)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ribboncontrol)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+    }
+}
---
+++ MEMS.Client.Produce/FormPInsList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MEMS.Client.Produce
+{
+    public partial class FormPInsList : MEMS.Client.Common.BaseListForm
+    {
+        public FormPInsList()
+        {
+            InitializeComponent();
+        }
+    }
+}
